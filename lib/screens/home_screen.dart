07ae850b422731a3a3ab3cee807815f2e5cb3588
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-02-25 07:23:40 +0530

    app theme is defined.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:thing_easy/utilites/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(backgroundColor: AppTheme.light);
   }
 }
--- lib/utilites/app_theme.dart
+++ lib/utilites/app_theme.dart
@@ -1,1 +1,9 @@
-class AppTheme {}
+import 'package:flutter/widgets.dart';
+
+class AppTheme {
+  static const Color light = Color(0xFFffffff);
+  static const Color dark = Color(0xFF1e1e1e);
+  static const Color grey = Color(0xFFf4f5f7);
+  static const Color subTextColor = Color(0xFF595c5e);
+  static const Color accentColor = Color(0xFF469ffc);
+}
