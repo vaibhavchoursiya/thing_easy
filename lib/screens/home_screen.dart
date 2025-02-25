ff8b358344d18232e3af2a9576e630a175c31e73
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-02-25 22:06:20 +0530

    quick find widget is created.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:thing_easy/utilites/app_theme.dart';
+import 'package:thing_easy/widgets/quick_find_field.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppTheme.light);
+    return Scaffold(
+      backgroundColor: AppTheme.light,
+      body: SafeArea(child: Column(children: [QuickFindField()])),
+    );
   }
 }
---
+++ lib/widgets/quick_find_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:thing_easy/utilites/app_theme.dart';
+
+class QuickFindField extends StatelessWidget {
+  const QuickFindField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Column(
+        children: [
+          TextField(
+            textAlign: TextAlign.center,
+            onChanged: (query) {},
+            decoration: InputDecoration(
+              constraints: BoxConstraints(maxHeight: 40.0),
+              contentPadding: const EdgeInsets.all(2),
+              filled: true,
+              fillColor: Color(0xFFe6e8ea),
+              hintText: "🔍 Quick Find",
+              hintStyle: GoogleFonts.inter(
+                color: AppTheme.subTextColor.withValues(alpha: 0.35),
+                fontWeight: FontWeight.w600,
+                fontSize: 16.0,
+              ),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
