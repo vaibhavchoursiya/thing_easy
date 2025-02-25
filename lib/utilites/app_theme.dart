509aa1e1844f2f2b9159967dc5b894e5f6f165df
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-02-25 22:14:18 +0530

    different styles

--- lib/utilites/app_theme.dart
+++ lib/utilites/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static const Color light = Color(0xFFffffff);
@@ -6,4 +7,25 @@ class AppTheme {
   static const Color grey = Color(0xFFf4f5f7);
   static const Color subTextColor = Color(0xFF595c5e);
   static const Color accentColor = Color(0xFF469ffc);
+
+  static final headingStyle = GoogleFonts.inter(
+    color: AppTheme.dark,
+    fontSize: 28.0,
+    fontWeight: FontWeight.w600,
+  );
+  static final subHeadingStyle = GoogleFonts.inter(
+    color: AppTheme.dark,
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+  );
+  static final titleStyle = GoogleFonts.inter(
+    color: AppTheme.dark.withValues(alpha: 0.7),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+  static final subTitleStyle = GoogleFonts.inter(
+    color: AppTheme.subTextColor,
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
 }
