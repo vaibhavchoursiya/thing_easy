ad107c99b9a691e7819c8f8fcb48eb0b99d1ae76
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-03-04 09:44:53 +0530

    imports

--- lib/screens/today_screen.dart
+++ lib/screens/today_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:thing_easy/utilites/app_theme.dart';
 
 class TodayScreen extends StatelessWidget {
   final List todayTasks;
@@ -12,6 +14,32 @@ class TodayScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppTheme.light,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30.0),
+            IconButton(
+              onPressed: () {
+                context.pop();
+              },
+              icon: Icon(
+                Icons.arrow_back_ios_outlined,
+                color: AppTheme.grey,
+                size: 28,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                children: [Text("⭐ Today", style: AppTheme.headingStyle)],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
